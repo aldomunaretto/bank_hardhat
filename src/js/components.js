184307bdcfca3a868497f471683d4f70f30f6447
Aldo Munaretto <aldo.munaretto@aggity.com>
2024-02-04 17:56:37 +0000

    Modifications on colors & background

--- src/js/components.js
+++ src/js/components.js
@@ -23,7 +23,9 @@ function Actions(props) {
 
         if (props.refresh) {
             props.refresh(wallet);
-        }
+            setDepositVal("0");
+            setWithdrawVal("0");
+        }  
     }
 
     async function withdraw(e) {
@@ -61,17 +63,17 @@ function Actions(props) {
     };
 
     return (
-        <div style={actionsStyle}>
+        <div className="numbers" style={actionsStyle}>
 
-            <div style={actionStyle}>
-                <h2>Deposit Process</h2>
-                <input type="number" name="depositVal" value={depositVal}
+            <div className="hello" style={actionStyle}>
+                <h2><span>Deposit</span> Process</h2>
+                <input id="number" type="number" name="depositVal" value={depositVal}
                     onChange={e => setDepositVal(e.target.value)} />
                 <button onClick={deposit}> Deposit </button>
             </div>
 
-            <div style={actionStyle}>
-                <h2>Withdraw Process</h2>
+            <div className="hello" style={actionStyle}>
+                <h2><span>Withdraw</span> Process</h2>
                 <input type="number" name="withdrawVal" value={withdrawVal}
                     onChange={e => setWithdrawVal(e.target.value)} />
                 <button onClick={withdraw}> Withdraw </button>
@@ -89,6 +91,7 @@ function Main(props) {
     const [wallet, setWallet] = React.useState("");
     const [ethBalance, setEthBalance] = React.useState("");
     const [bankBalance, setBankBalance] = React.useState("");
+    const [interestReceived, setinterestReceived] = React.useState("");
 
     async function refreshBalances(wallet) {
 
@@ -99,8 +102,14 @@ function Main(props) {
 
         // Get Your bank balance
 
-        const bankBalanceVal = await BANK.methods.getBalance().call({ from: wallet });
+        const bankBalanceVal = await BANK.methods.getMyBalance().call({ from: wallet });
         setBankBalance(web3.utils.fromWei(bankBalanceVal, 'ether'));
+
+        // Get Your bank balance
+
+        const interestReceivedVal = await BANK.methods.getMyInterest().call({ from: wallet });
+        setinterestReceived(web3.utils.fromWei(interestReceivedVal, 'ether'));
+        console.log(interestReceivedVal);
     }
 
     async function connectToMetamask(e) {
@@ -131,8 +140,8 @@ function Main(props) {
     return (
         <div>
 
-            <img src="https://keepcoding.io/wp-content/uploads/2022/01/cropped-logo-keepcoding-Tech-School.png"></img>
-            <h1>Welcome to Keepcoding Bank</h1>
+            <img src="./assets/logo.png" width="15%"></img>
+            <h1>Welcome to Bit Bank</h1>
 
             {
                 wallet ?
@@ -141,6 +150,7 @@ function Main(props) {
                         <p><b>Wallet</b> = {wallet}</p>
                         <p><b>Wallet Balance</b> = {ethBalance} ETH</p>
                         <p><b>Bank Balance</b> = {bankBalance} ETH</p>
+                        <p><b>Interest Earned</b> = {interestReceived} ETH</p>
                         <Actions wallet={wallet} refresh={refreshBalances} />
                     </section>
 
--- src/js/index.js
+++ src/js/index.js
@@ -15,24 +15,217 @@ if (typeof window.ethereum == 'undefined') {
 
     const web3 = new window.Web3(METAMASK);
 
-    const BANK_ADDRESS = {CONTRACT_ADDRESS};
-    const BANK_ABI = {CONTRACT_ABI};
+    const BANK_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+    const BANK_ABI =  [
+      {
+        "inputs": [],
+        "stateMutability": "nonpayable",
+        "type": "constructor"
+      },
+      {
+        "anonymous": false,
+        "inputs": [
+          {
+            "indexed": true,
+            "internalType": "address",
+            "name": "depositor",
+            "type": "address"
+          },
+          {
+            "indexed": false,
+            "internalType": "uint256",
+            "name": "amount",
+            "type": "uint256"
+          }
+        ],
+        "name": "Deposited",
+        "type": "event"
+      },
+      {
+        "anonymous": false,
+        "inputs": [
+          {
+            "indexed": true,
+            "internalType": "address",
+            "name": "user",
+            "type": "address"
+          },
+          {
+            "indexed": false,
+            "internalType": "uint256",
+            "name": "amount",
+            "type": "uint256"
+          }
+        ],
+        "name": "InterestPaid",
+        "type": "event"
+      },
+      {
+        "anonymous": false,
+        "inputs": [
+          {
+            "indexed": true,
+            "internalType": "address",
+            "name": "withdrawal",
+            "type": "address"
+          },
+          {
+            "indexed": false,
+            "internalType": "uint256",
+            "name": "amount",
+            "type": "uint256"
+          }
+        ],
+        "name": "Withdrawn",
+        "type": "event"
+      },
+      {
+        "inputs": [],
+        "name": "deposit",
+        "outputs": [],
+        "stateMutability": "payable",
+        "type": "function"
+      },
+      {
+        "inputs": [],
+        "name": "getMyBalance",
+        "outputs": [
+          {
+            "internalType": "uint256",
+            "name": "",
+            "type": "uint256"
+          }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "inputs": [],
+        "name": "getMyInterest",
+        "outputs": [
+          {
+            "internalType": "uint256",
+            "name": "",
+            "type": "uint256"
+          }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "inputs": [
+          {
+            "internalType": "address",
+            "name": "user",
+            "type": "address"
+          }
+        ],
+        "name": "getUserBalance",
+        "outputs": [
+          {
+            "internalType": "uint256",
+            "name": "",
+            "type": "uint256"
+          }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "inputs": [
+          {
+            "internalType": "address",
+            "name": "user",
+            "type": "address"
+          }
+        ],
+        "name": "getUserInterest",
+        "outputs": [
+          {
+            "internalType": "uint256",
+            "name": "",
+            "type": "uint256"
+          }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "inputs": [
+          {
+            "internalType": "address",
+            "name": "user",
+            "type": "address"
+          }
+        ],
+        "name": "getUserLastInterestPaid",
+        "outputs": [
+          {
+            "internalType": "uint256",
+            "name": "",
+            "type": "uint256"
+          }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "inputs": [],
+        "name": "getannualInterestRate",
+        "outputs": [
+          {
+            "internalType": "uint256",
+            "name": "",
+            "type": "uint256"
+          }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "inputs": [
+          {
+            "internalType": "uint256",
+            "name": "rate",
+            "type": "uint256"
+          }
+        ],
+        "name": "setannualInterestRate",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+      },
+      {
+        "inputs": [
+          {
+            "internalType": "uint256",
+            "name": "amount",
+            "type": "uint256"
+          }
+        ],
+        "name": "withdraw",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+      }
+    ];
 
     BANK = new web3.eth.Contract(BANK_ABI, BANK_ADDRESS);
 
     NETWORK = {
-        chainName: 'Hardhat',
+        chainName: 'Hardhat DEV',
         chainId: web3.utils.toHex(31337),
         nativeCurrency: { name: 'ETH', decimals: 18, symbol: 'ETH' },
         rpcUrls: ['http://127.0.0.1:8545']
     }
 
-    function MyApp() {
-        return <div><Main /></div>;
-    }
+    // function MyApp() {
+    //     return <div><Main /></div>;
+    // }
 
     const container = document.getElementById('root');
     const root = ReactDOM.createRoot(container);
 
-    root.render(<MyApp />);
+    // root.render(<MyApp />);
+    root.render(<Main />);
 }
