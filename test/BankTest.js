4d39fe049d10c5d4eb660520977d0a1ce2685d3a
Aldo Munaretto <aldo.munaretto@aggity.com>
2024-02-04 11:27:23 +0000

    Adding new tests to cover the new functionalities

--- test/BankTest.js
+++ test/BankTest.js
@@ -1,4 +1,4 @@
-const { assert, expect } = require('chai')
+const { assert, expect } = require('chai');
 
 describe("Nuestro Banco", function () {
 
@@ -86,6 +86,56 @@ describe("Nuestro Banco", function () {
 
         userBalance = await this.bank.connect(this.owner).getUserBalance(this.account1);
         assert.equal(userBalance.toString(), "50", "Invalid Balance");
+    });
+
+    it("Interest is calculated correctly", async function () {
+
+        await this.bank.connect(this.account1).deposit({ value: ethers.parseEther("1")});
+
+        await new Promise(resolve => setTimeout(resolve, 1000));
+
+        await this.bank.connect(this.account1).deposit({ value: ethers.parseEther("1")});
+
+        let interest = await this.bank.connect(this.owner).getUserInterest(this.account1);
+        // console.log(interest.toString());
+        // revisar esto ya que segun la calculadora de intereses deberia ser 15854895991.9
+        assert(interest.toString(), "1585489599", "interest is calculated correctly");
+    });
+
+    it("Timestamp for last interest paid is set correctly", async function () {
+
+        await this.bank.connect(this.account1).deposit({ value: 10 });
+
+        let timestamp = await this.bank.connect(this.owner).getUserLastInterestPaid(this.account1);
+        assert(timestamp.toString() > 0, "timestamp is not set correctly");
+    });
+
+    it("Timestamp difference is calculated correctly", async function () {
+
+        await this.bank.connect(this.account1).deposit({ value: 10 });
+
+        let timestamp1 = await this.bank.connect(this.owner).getUserLastInterestPaid(this.account1);
+
+        await new Promise(resolve => setTimeout(resolve, 1000));
+
+        await this.bank.connect(this.account1).deposit({ value: 10 });
+
+        let timestamp2 = await this.bank.connect(this.owner).getUserLastInterestPaid(this.account1);
+        assert.equal((timestamp2-timestamp1).toString(), "1", "timestamp difference is not calculated correctly");
+    });
+
+    it("Annual interest rate can be set by admin", async function () {
+
+        await this.bank.connect(this.owner).setannualInterestRate(10);
+
+        let annualRate = await this.bank.getannualInterestRate();
+        assert.equal(annualRate.toString(), "10", "interest rate is not set correctly");
+    });
+
+    it("Annual interest rate cannot be set by non-admin", async function () {
+
+        await expect(this.bank.connect(this.account1).setannualInterestRate(10))
+            .to.be.revertedWith("UNAUTHORIZED");
     });
 
 });
