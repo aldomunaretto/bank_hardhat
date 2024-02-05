ff13c7063b69815529b8576803fa690a218b4571
Aldo Munaretto <aldo.munaretto@aggity.com>
2024-02-05 13:37:16 +0000

    Allow automining on Hardhat Node

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,4 +3,13 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+      mining: {
+        auto: true,
+        interval: 5000 // Minado cada 5 segundo
+      }
+    }
+  }
 };
